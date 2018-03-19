5228f383d381dbcab79acd1baac75f7f7dbb8fdb
Мурадов Музафар Алиевич <m.muradov@butik.ru>
2018-03-19 12:35:20 +0300

    removed redundant stuff

--- src/main/java/ru/sepnotican/printserver/servlet/PrintServlet.java
+++ src/main/java/ru/sepnotican/printserver/servlet/PrintServlet.java
@@ -16,9 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class PrintServlet extends HttpServlet {
 
@@ -51,25 +48,6 @@ public class PrintServlet extends HttpServlet {
         resp.setContentType("application/json");
         resp.getWriter().print(PrintingHandler.getInstance().getPrinterListInJson());
         resp.setStatus(HttpServletResponse.SC_OK);
-    }
-
-
-    static byte[] asaved = null;
-
-    public static String getMD5(byte[] input) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(input);
-            BigInteger number = new BigInteger(1, messageDigest);
-            String hashtext = number.toString(16);
-            // Now we need to zero pad it if you actually want the full 32 chars.
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
